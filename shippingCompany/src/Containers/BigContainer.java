5ab2e179a7508dfa31edf20fc47a0b051c398f93
Samuel <spantoja0208@gmail.com>
2022-06-05 17:05:05 +0100

    fixed containers

---
+++ shippingCompany/src/Containers/BigContainer.java
@@ -0,0 +1,21 @@
+package Containers;
+
+public class BigContainer {
+
+	private double containerVolume = 75.587337;
+
+	public BigContainer(double containerVolume) {
+		super();
+		this.containerVolume = containerVolume;
+	}
+
+	public double getContainerVolume() {
+		return containerVolume;
+	}
+
+	public void setContainerVolume(double containerVolume) {
+		this.containerVolume = containerVolume;
+	}
+	
+	
+}
---
+++ shippingCompany/src/Containers/SmallContainer.java
@@ -0,0 +1,21 @@
+package Containers;
+
+public class SmallContainer {
+	
+	private double containerVolume = 38.139822;
+
+	public SmallContainer(double containerVolume) {
+		super();
+		this.containerVolume = containerVolume;
+	}
+
+	public double getContainerVolume() {
+		return containerVolume;
+	}
+
+	public void setContainerVolume(double containerVolume) {
+		this.containerVolume = containerVolume;
+	}
+	
+	
+}
